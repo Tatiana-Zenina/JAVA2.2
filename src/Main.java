2da0af8c3bf5aa4f2ba0d95e8a911c32802477d9
HW Netology <zenina_t@inbox.ru>
2023-04-26 22:44:29 +0400

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        int clientBalance = 400;
+        int additionToBalance = 1200;
+        int bonus = 0;
+
+        if (additionToBalance > 1000) {
+            bonus = additionToBalance / 100;
+
+        }
+
+        int newClientBalance = clientBalance + additionToBalance + bonus;
+
+        System.out.println("Текущий Баланс:" + newClientBalance);
+        System.out.println("Бонус за пополнение:" + bonus);
+    }
+
 }
